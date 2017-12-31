49df3d966654cefca908dfb784a9d86ff2f6033c
Alexander Tekle <tekle.alexander@gmail.com>
2017-12-31 02:05:18 -0600

    Finished

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,19 +1,53 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
+var mongoose = require('mongoose'); //database
 
-var data = [{item: 'get milk'}, {item: 'do homework'}, {item: 'walk dog'}];
+//connect to the database
 
+mongoose.connect('mongodb://test:test@ds135537.mlab.com:35537/todo');
+//create a scheme - this is like a blueprint
+
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+/*var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+  if (err) throw err;
+  console.log('item saved');
+});*/
+
+//var data = [{item: 'get milk'}, {item: 'do homework'}, {item: 'walk dog'}]; don't need this anymore bc we will pull from database
 module.exports = function(app){
   app.get('/todo', function(req,res){
-    res.render('todo', {todos: data});
+    //get data from mongodb and pass it to the view
+    //console.log("beginning get");
+    Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
+    //console.log('done.');
+
   });
 
   app.post('/todo', urlencodedParser, function(req,res){
-    data.push(req.body);
-    res.json(data);
+
+    //get data from the view and add it to the mongodb
+    var newTodo = Todo(req.body).save(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    })
+    //data.push(req.body);
   });
 
-  app.delete('/todo', function(req,res){
+  app.delete('/todo/:item', function(req,res){
+    //delete the requested item from mongodb
+
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
 
   });
 };
